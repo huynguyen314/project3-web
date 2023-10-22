e6235fa5511888c49213b691ab3ce0685e71f9e7
huynguyen314 <thanhhuya10lk@gmail.com>
2023-10-22 18:51:44 +0700

    Update config file

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig:
     DEBUG = True
     POSTGRES_URL="project3-postgreserver.postgres.database.azure.com"  #TODO: Update value
-    POSTGRES_USER="postgresadmin@project3-postgreserver" #TODO: Update value
-    POSTGRES_PW="Foryou@postgres"   #TODO: Update value
+    POSTGRES_USER="postgresadmin" #TODO: Update value
+    POSTGRES_PW="Foryou@huy"   #TODO: Update value
     POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://huynotification.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=27Q6FUCFDeSbbnCfLDq6/ahwEV99bsSIq+ASbCGDD3o=' #TODO: Update value
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://huynotification.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=KkSg9laoaz5+CMo/G+JvzK4TqxLIudw2k+ASbNThSM8=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
